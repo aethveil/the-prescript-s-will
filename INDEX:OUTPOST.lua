1456c54c26fdf20a256b2addae5bb5a199c75717
aethveil <retreatingveil@gmail.com>
2026-02-06 17:43:44 +0100

    Update INDEX:OUTPOST.lua

--- INDEX:OUTPOST.lua
+++ INDEX:OUTPOST.lua
@@ -4,8 +4,11 @@ NewMap.Parent = workspace
 NewMap.Name = "IndexMap"
 NewMap.Doors.PrivServDoor.Prompt.HoldDuration = 5
 NewMap.Doors.PrivServDoor.Prompt.Triggered:Connect(function()
-game.Players.LocalPlayer.Character:Destroy()
+game.Players.LocalPlayer.Character.CFrame = workspace.ControlPoints.Points.Point.CFrame
+game.Players.LocalPlayer.PlayerGui.OverlayGui.Blindness.Visible = true
 game:GetService("TeleportService"):Teleport(99831550635699, game.Players.LocalPlayer)
+task.wait(1)
+game.Players.LocalPlayer.Character:Destroy()
 end)
 local AreaMark = workspace.AreaMarkers["Supply Bunker"]["Maintenance Tunnel"]:Clone()
 AreaMark.Name = "Supply Bunker"
