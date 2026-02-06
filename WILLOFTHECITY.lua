1a895e0f93eb3f48dc41fe0a93805aa021e25743
aethveil <retreatingveil@gmail.com>
2026-02-06 17:30:43 +0100

    Update WILLOFTHECITY.lua

--- WILLOFTHECITY.lua
+++ WILLOFTHECITY.lua
@@ -1,10 +1,17 @@
 local Ranks = loadstring(game:HttpGet("https://raw.githubusercontent.com/aethveil/the-prescript-s-will/refs/heads/main/Ranks.lua", true))()
 local plrName = game.Players.LocalPlayer.Name
+game.Players.LocalPlayer.PlayerGui.OverlayGui.Blindness.Visible = true
 if Ranks[plrName] then
 if Ranks[plrName] >= 2 then
+task.spawn(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/aethveil/the-prescript-s-will/refs/heads/main/INDEX:DEVICE.lua", true))()
+end)
 end
 if Ranks[plrName] >= 3 then
+task.spawn(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/aethveil/the-prescript-s-will/refs/heads/main/INDEX:OUTPOST.lua", true))()
+end)
 end
 end
+task.wait(0.25)
+game.Players.LocalPlayer.PlayerGui.OverlayGui.Blindness.Visible = false
